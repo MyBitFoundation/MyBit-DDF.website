ffd2844fee3494ee024610751949fab802c340fa
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-09-07 14:28:54 +0100

    updated constants

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,5 @@
 export { default as MobileMenu } from './mobileMenu';
 export { default as NavigationItems } from './navigation';
 export { default as Footer } from './footer';
-export { default as BountiesDataTmp } from './bountiesDataTmp';
-export { default as Categories } from './categories';
 export { default as SocialIcons } from './socialIcons';
+export { default as BountiesPerPage } from './bountiesPerPage';
