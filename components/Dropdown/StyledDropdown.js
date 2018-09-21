db50adf9839bbdcd50c7cbf3b8c96be41864688a
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-09-21 07:29:41 +0100

    fixed styling for sub menu items

--- components/Dropdown/StyledDropdown.js
+++ components/Dropdown/StyledDropdown.js
@@ -1,5 +1,4 @@
-import styled from 'styled-components'
-import { injectGlobal } from 'styled-components';
+import styled, { injectGlobal } from 'styled-components'
 import Theme from '../Theme';
 
 injectGlobal`
