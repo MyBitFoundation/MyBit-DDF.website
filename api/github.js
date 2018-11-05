24625b0e1d990c473d3e4b793ac25e4dbd13d674
Valeriy Tverdohleb <v@tverdohleb.com>
2018-11-05 18:27:31 +0200

    create local express server for sending mock data in offline

--- api/github.js
+++ api/github.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
-const END_POINT = "http://localhost:9001"
+const END_POINT = "https://github.mybit.io";
 
 import getConfig from 'next/config';
 
 const GithubApi = {
   getOrgIssues: async () => {
     const { publicRuntimeConfig } = getConfig();
-    const url = publicRuntimeConfig.offlineMode ? "/api/local-issues" : "/api-issues";
-    const response = await axios(END_POINT + url)
+    const isOffline = publicRuntimeConfig.offlineMode;
+    const url = isOffline 
+      ? `/api/local-issues`
+      : `${END_POINT}/api/issues `;
+      
+    const response = await axios(url)
     return response.data;
   },
 }
