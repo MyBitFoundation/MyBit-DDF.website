521be3b47473f9494bc85a4684106310d9b79b98
Cryptomental <cryptomental.com@gmail.com>
2018-11-30 22:45:17 +0100

    components/Bounties/card: Add StyledDescription.
    Add StyledDescription to show DDF task descriptions in.
    
    Refs: https://github.com/MyBitFoundation/MyBit-DDF.website/issues/94

---
+++ components/Bounties/card/StyledDescription.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const StyledDescription = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding-top: 15px;
+`;
+
+export default StyledDescription;
